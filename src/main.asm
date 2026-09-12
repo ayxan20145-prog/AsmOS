4066437cc88c268fe585df5faa40d9ea994189dd
ayxan <ayxan.20145@gmail.com>
2026-09-12 17:24:29 +0400

    clear screen and print Hi

--- src/main.asm
+++ src/main.asm
@@ -4,9 +4,25 @@ section .text
 kernel_main:
         mov edi, 0xb8000
 
+        call clear
+        
         mov [edi], 'H'
         mov [edi + 1], 0x0F
         mov [edi + 2], 'i'
         mov [edi + 3], 0x0F
 
         hlt
+
+clear:
+        xor edx, edx
+.loop:
+        cmp edx, 2000
+        je .done
+
+        mov [edi + edx * 2], ' '
+        mov [edi + edx * 2 + 1], 0x0F
+
+        inc edx
+        jmp .loop
+.done:
+        ret
