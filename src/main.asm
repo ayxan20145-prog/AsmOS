4b644f72039fe82c3455b94d8a925bb97f6bca7d
ayxan <ayxan.20145@gmail.com>
2026-09-12 17:17:25 +0400

    print hi to vga buffer

--- src/main.asm
+++ src/main.asm
@@ -2,4 +2,11 @@ section .text
         global kernel_main
 
 kernel_main:
+        mov edi, 0xb8000
+
+        mov [edi], 'H'
+        mov [edi + 1], 0x0F
+        mov [edi + 2], 'i'
+        mov [edi + 3], 0x0F
+
         hlt
